67a44c69232122a7db01bd178220c3f23aa5dcf1
Kaustubh_Shahapurkar2002 <kaustubh.b.shahapurkar@gmail.com>
2022-10-11 20:27:18 +0530

    Initializing and filling of columns

--- tut04/tut04.py
+++ tut04/tut04.py
@@ -25,3 +25,39 @@ velocity.loc[((velocity.u_ < 0) & (velocity.v_ < 0) & (velocity.w_ > 0)), "Octan
 velocity.loc[((velocity.u_ < 0) & (velocity.v_ < 0) & (velocity.w_ < 0)), "Octant"] = "-3"
 velocity.loc[((velocity.u_ > 0) & (velocity.v_ < 0) & (velocity.w_ > 0)), "Octant"] = "+4"
 velocity.loc[((velocity.u_ > 0) & (velocity.v_ < 0) & (velocity.w_ < 0)), "Octant"] = "-4"
+
+
+LongSub = {"-1" : 0, "+1" : 0 ,  "-2" : 0 , "+2" : 0 , "-3" : 0 , "+3" : 0 , "-4" : 0, "+4" : 0}
+
+# The column of the files have to be initialized now... we will do so
+
+velocity[" "]= " "
+velocity["count"] = ""
+velocity["Longest Subsquence Length"] = ""
+velocity["Count"] = ""
+    
+velocity["  "] = " "
+velocity["count2"] = ""
+velocity["Longest Subsquence Length2"] = ""
+velocity["Count2"] = ""
+
+ # In the following while loop I have basically filled the columns
+
+var = 0 # Counting longest Subsequence Length 
+while var <= num:
+    octant1 = velocity["Octant"][var]
+    curr_cnt = 0
+    j = var
+    while j <= num:
+        #adding some try except here
+        try:
+            octant2= velocity["Octant"][j]
+            if(octant2 == octant1):
+                curr_cnt +=1 
+            else:
+                break
+            j += 1
+        except:
+            print("row not found")
+    LongSub[velocity["Octant"][var]] = max(LongSub[velocity["Octant"][var]], curr_cnt)
+    var = j
