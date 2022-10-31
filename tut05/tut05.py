9a4bcd194c3de404fa6b259c7c5cec8a253e4c1f
Kaustubh_Shahapurkar2002 <kaustubh.b.shahapurkar@gmail.com>
2022-10-31 14:09:53 +0530

    Uptill the count of occurences in each ocatant

--- tut05/tut05.py
+++ tut05/tut05.py
@@ -1,3 +1,59 @@
 from datetime import datetime
 from decimal import DivisionByZero
 start_time = datetime.now()
+
+from platform import python_version
+import pandas as pd
+ver = python_version()
+
+if ver == "3.8.10":
+    print("Correct Version Installed")
+else:
+    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+
+# reading the input file
+velocity = pd.read_excel('octant_input.xlsx')
+
+# Calcuation of the mean or average values
+velocity.at[0, 'U Avg'] = velocity['U'].mean()
+velocity.at[0, 'V Avg'] = velocity['V'].mean()
+velocity.at[0, 'W Avg'] = velocity['W'].mean()
+
+#The difference between the value and the mean 
+velocity["U'=U - U avg"] = velocity['U'] - velocity['U Avg'][0]
+velocity["V'=V - V avg"] = velocity['V'] - velocity['V Avg'][0]
+velocity["W'=W - W avg"] = velocity['W'] - velocity['W Avg'][0]
+
+#The value of the respective octants
+for i in range(len(velocity)):
+    if(velocity["U'=U - U avg"][i] > 0 and velocity["V'=V - V avg"][i] > 0):
+        velocity.at[i, 'Octant'] = int(1)
+    if(velocity["U'=U - U avg"][i] < 0 and velocity["V'=V - V avg"][i] > 0):
+        velocity.at[i, 'Octant'] = int(2)
+    if(velocity["U'=U - U avg"][i] < 0 and velocity["V'=V - V avg"][i] < 0):
+        velocity.at[i, 'Octant'] = int(3)
+    if(velocity["U'=U - U avg"][i] > 0 and velocity["V'=V - V avg"][i] < 0):
+        velocity.at[i, 'Octant'] = int(4)
+    if(velocity["W'=W - W avg"][i] < 0):
+        velocity.at[i, 'Octant'] = velocity['Octant'][i] * -1
+
+# 'User Input' and 'Overall Count'
+velocity.at[1, ''] = 'User Input'
+velocity.at[0, 'Octant ID'] = 'Overall Count'
+
+#to find the overall octant counts and each mod ranges we have the following dictionary
+octant_counts = {'overall': {}, 'mod_ranges': {}}
+
+#The count of all the occurance in each octant 
+for oct in [1, -1, 2, -2, 3, -3, 4, -4]:
+    octant_counts['overall'][oct] = velocity['Octant'].value_counts()[oct]
+    velocity.at[0, oct] = octant_counts['overall'][oct]
+
+# mod value
+    
+mod=5000 
+
+#
+# a function which I will define later in commits
+octant_range_names(mod)
+velocity.to_excel('octant_output_ranking_excel.xlsx', index = None)
