bcb1a244475457858bdbcaa3bdd0b86ea318659b
Kaustubh_Shahapurkar2002 <kaustubh.b.shahapurkar@gmail.com>
2022-10-31 15:29:21 +0530

    Octant range names function

--- tut05/tut05.py
+++ tut05/tut05.py
@@ -3,6 +3,49 @@ from decimal import DivisionByZero
 start_time = datetime.now()
 
 
+def octant_range_names(mod=5000):
+    #A function to find the count in each intervals
+    octact_identification(mod)
+
+    #A diactionary 'octant_name_id_map' 
+    octant_name_id_map = {1:"Internal Outward Interaction", -1:"External Outward Interaction", 2:"External Ejection", -2:"Internal Ejection", 3:"External Inward Interaction", -3:"Internal Inward Interaction", 4:"Internal Sweep", -4:"External Sweep"}
+    
+    #The oct_rank dictionary used as a map
+    oct_rank = {key: rank for rank, key in enumerate(sorted(octant_counts['overall'], key=octant_counts['overall'].get, reverse=True), 1)}
+
+    #The rank of each octant is added to the output file
+    for oct in [1, -1, 2, -2, 3, -3, 4, -4]:
+        velocity.at[0, f'Rank of {oct}'] = oct_rank[oct]
+    
+    #Adding Octant ID of octant having rank 1
+    velocity.at[0, 'Octant ID of Rank 1'] = list(oct_rank.keys())[list(oct_rank.values()).index(1)]
+    
+    #The Octant name -> octant id
+    velocity.at[0, 'Octant Name of Rank 1'] = octant_name_id_map[list(oct_rank.keys())[list(oct_rank.values()).index(1)]]
+
+    # The ranks of each octant, Octant ID and name of the octant with Rank 1 is added to the output file for each mod range
+    for i, (mod_range, octant_count) in enumerate(octant_counts['mod_ranges'].items()):
+        oct_rank = {key: rank for rank, key in enumerate(sorted(octant_count, key=octant_count.get, reverse=True), 1)}
+        for oct in [1, -1, 2, -2, 3, -3, 4, -4]:
+            velocity.at[2 + i, f'Rank of {oct}'] = oct_rank[oct]
+        velocity.at[2 + i, 'Octant ID of Rank 1'] = list(oct_rank.keys())[list(oct_rank.values()).index(1)]
+        velocity.at[2 + i, 'Octant Name of Rank 1'] = octant_name_id_map[list(oct_rank.keys())[list(oct_rank.values()).index(1)]]
+
+    velocity.at[11, 1] = 'Octant ID'
+    velocity.at[11, -1] = 'Octant Name'
+    velocity.at[11, 2] = 'Count of Rank 1 Mod Values'
+    
+    # The total number of occurences of Rank 1
+    for i, oct in enumerate([1, -1, 2, -2, 3, -3, 4, -4]):
+        velocity.at[12 + i, 1] = oct
+        velocity.at[12 + i, -1] = octant_name_id_map[oct]
+        counts_rank1 = 0
+        for mod_range, octant_count in octant_counts['mod_ranges'].items():
+            oct_rank = {key: rank for rank, key in enumerate(sorted(octant_count, key=octant_count.get, reverse=True), 1)}
+            if oct_rank[oct] == 1:
+                counts_rank1 += 1
+        velocity.at[12 + i, 2] = counts_rank1
+
 try:
     from platform import python_version
     import pandas as pd
@@ -72,3 +115,5 @@ except ImportError:
 end_time = datetime.now()
 #total duration of the execution of our program
 print('Duration of Program Execution: {}'.format(end_time - start_time))
+
+
