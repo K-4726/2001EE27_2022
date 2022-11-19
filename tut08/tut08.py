bd73bca2e5107f08081fce048b2907c2d1feb267
Kaustubh_Shahapurkar2002 <kaustubh.b.shahapurkar@gmail.com>
2022-11-19 18:39:31 +0530

    scoreboard creation and loop writing

--- tut08/tut08.py
+++ tut08/tut08.py
@@ -8,6 +8,31 @@ def scorecard():
 	#the informationon the scoreboard will be stored in this dictionary
 	innings_01 = {'runs': 0, 'wickets': 0, 'balls': 0, 'batting': {player : {'runs': 0, 'balls': 0, '4s': 0, '6s': 0, 'Status': ''} for player in pak_players}, 'bowling': {player : {'runs': 0, 'balls': 0, 'maidens': 0, 'wickets': 0, 'nb': 0, 'wide': 0} for player in ind_players}, 'Extras': {'byes': 0, 'nb': 0, 'lb': 0, 'wide': 0, 'pty': 0}, 'dnb': pak_players.copy(), 'fow': [], 'powerplay': 0}
 	innings_02 = {'runs': 0, 'wickets': 0, 'balls': 0, 'batting': {player : {'runs': 0, 'balls': 0, '4s': 0, '6s': 0, 'Status': ''} for player in ind_players}, 'bowling': {player : {'runs': 0, 'balls': 0, 'maidens': 0, 'wickets': 0, 'nb': 0, 'wide': 0} for player in pak_players}, 'Extras': {'byes': 0, 'nb': 0, 'lb': 0, 'wide': 0, 'pty': 0}, 'dnb': ind_players.copy(), 'fow': [], 'powerplay': 0}
+    
+    #For finding maiden over we have to store runs gave by the bowler_curr before giving the over
+	initial=-1
+
+	#Creating output of scoreboard by reading the inning files
+	with open('pak_inns1.txt', 'r') as pakistan_inns, open('india_inns2.txt', 'r') as india_inns, open('output.txt','w') as output:
+		# Input file reading is done now
+		tot_lines1 = pakistan_inns.readlines()
+		tot_lines2 = india_inns.readlines()
+
+		# Let's define a variable cnt to mark the player who is batting
+		cnt=0
+		print("", file=output)
+
+		# Now the following loops will go through the innings of both the teams
+		for innings, lines in zip([innings_01, innings_02], [tot_lines1, tot_lines2]):
+
+			#This loop, loops through the lines of the comment section for getting the information	
+			for info in [line for line in lines if line.strip()]:
+
+				#extracting the names of the batsmen and bowler, result and the over using the information in line
+				batter_curr= getPlayer(info.split(",")[0].split(" ", 1)[1].split("to")[1].strip())
+				bowler_curr = getPlayer(info.split(",")[0].split(" ", 1)[1].split("to")[0].strip())
+				outcome = info.split(",")[1].strip()
+				over=info.split(",")[0].split(" ", 1)[0].strip()
 
 
 from platform import python_version
