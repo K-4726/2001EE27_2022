a28464a5aa7fadadfef8be776eb6a604d1f95c03
Kaustubh_Shahapurkar2002 <kaustubh.b.shahapurkar@gmail.com>
2022-11-19 21:24:13 +0530

    Output and final lines

--- tut08/tut08.py
+++ tut08/tut08.py
@@ -1,14 +1,18 @@
 from datetime import datetime
 start_time = datetime.now()
 
+#Function to get the name of the player as registered in the team
+def getPlayer(player):
+	for person in all_players:
+		if player in person:
+			return person
+
 def scorecard():
-    
     print("The function was called sucessfully.")
     print("Please wait while the output file is created.")
 	#the informationon the scoreboard will be stored in this dictionary
-	innings_01 = {'runs': 0, 'wickets': 0, 'balls': 0, 'batting': {player : {'runs': 0, 'balls': 0, '4s': 0, '6s': 0, 'Status': ''} for player in pak_players}, 'bowling': {player : {'runs': 0, 'balls': 0, 'maidens': 0, 'wickets': 0, 'nb': 0, 'wide': 0} for player in ind_players}, 'Extras': {'byes': 0, 'nb': 0, 'lb': 0, 'wide': 0, 'pty': 0}, 'dnb': pak_players.copy(), 'fow': [], 'powerplay': 0}
-	innings_02 = {'runs': 0, 'wickets': 0, 'balls': 0, 'batting': {player : {'runs': 0, 'balls': 0, '4s': 0, '6s': 0, 'Status': ''} for player in ind_players}, 'bowling': {player : {'runs': 0, 'balls': 0, 'maidens': 0, 'wickets': 0, 'nb': 0, 'wide': 0} for player in pak_players}, 'Extras': {'byes': 0, 'nb': 0, 'lb': 0, 'wide': 0, 'pty': 0}, 'dnb': ind_players.copy(), 'fow': [], 'powerplay': 0}
-    
+    innings1 = {'runs': 0, 'wickets': 0, 'balls': 0, 'batting': {player : {'runs': 0, 'balls': 0, '4s': 0, '6s': 0, 'status': ''} for player in pak_players}, 'bowling': {player : {'runs': 0, 'balls': 0, 'maidens': 0, 'wickets': 0, 'nb': 0, 'wide': 0} for player in ind_players}, 'extras': {'byes': 0, 'nb': 0, 'lb': 0, 'wide': 0, 'pty': 0}, 'dnb': pak_players.copy(), 'fow': [], 'powerplay': 0}
+    innings2 = {'runs': 0, 'wickets': 0, 'balls': 0, 'batting': {player : {'runs': 0, 'balls': 0, '4s': 0, '6s': 0, 'status': ''} for player in ind_players}, 'bowling': {player : {'runs': 0, 'balls': 0, 'maidens': 0, 'wickets': 0, 'nb': 0, 'wide': 0} for player in pak_players}, 'extras': {'byes': 0, 'nb': 0, 'lb': 0, 'wide': 0, 'pty': 0}, 'dnb': ind_players.copy(), 'fow': [], 'powerplay': 0}
     #For finding maiden over we have to store runs gave by the bowler_curr before giving the over
 	initial=-1
 
@@ -23,7 +27,7 @@ def scorecard():
 		print("", file=output)
 
 		# Now the following loops will go through the innings of both the teams
-		for innings, lines in zip([innings_01, innings_02], [tot_lines1, tot_lines2]):
+		for innings, lines in zip([innings1, innings2], [tot_lines1, tot_lines2]):
 
 			#This loop, loops through the lines of the comment section for getting the information	
 			for info in [line for line in lines if line.strip()]:
@@ -174,7 +178,7 @@ def scorecard():
 						if outcome == "byes":
 							innings['Extras']['byes'] += 4
 						else:
-				    			innings['Extras']['lb'] += 4
+				    		innings['Extras']['lb'] += 4
                 
                 # if condition checks if the runs given to the batsman by the bowler by the end of the over
 				
@@ -235,8 +239,8 @@ else:
 	print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
 
 # we define all the players of both the teams
-india_players = ['Rohit Sharma (c)', 'KL Rahul', 'Virat Kohli', 'Suryakumar Yadav', 'Dinesh Karthik (w)', 'Hardik Pandya', 'Ravindra Jadeja', 'Bhuvneshwar Kumar', 'Avesh Khan', 'Yuzvendra Chahal', 'Arshdeep Singh']
-pakistan_players = ['Babar Azam (c)', 'Mohammad Rizwan (w)', 'Fakhar Zaman', 'Iftikhar Ahmed', 'Khushdil Shah', 'Asif Ali', 'Shadab Khan', 'Mohammad Nawaz', 'Naseem Shah', 'Haris Rauf', 'Shahnawaz Dahani']
+ind_players = ['Rohit Sharma (c)', 'KL Rahul', 'Virat Kohli', 'Suryakumar Yadav', 'Dinesh Karthik (w)', 'Hardik Pandya', 'Ravindra Jadeja', 'Bhuvneshwar Kumar', 'Avesh Khan', 'Yuzvendra Chahal', 'Arshdeep Singh']
+pak_players = ['Babar Azam (c)', 'Mohammad Rizwan (w)', 'Fakhar Zaman', 'Iftikhar Ahmed', 'Khushdil Shah', 'Asif Ali', 'Shadab Khan', 'Mohammad Nawaz', 'Naseem Shah', 'Haris Rauf', 'Shahnawaz Dahani']
 all_players = ind_players + pak_players
 
 #calling the function
@@ -244,3 +248,8 @@ try:
  scorecard()
 except:
  print("Function was not called correctly.")
+
+#This shall be the last lines of the code.
+end_time = datetime.now()
+print("Output files are ready :] ")
+print('Duration of Program Execution: {}'.format(end_time - start_time))
